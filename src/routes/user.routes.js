9c4cd8bd96b03b868919ba92e3bd07f60a3db979
leminhduc22072003 <114459673+leminhduc22072003@users.noreply.github.com>
2023-06-10 11:31:03 +0700

    thay doi index

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,6 +3,6 @@ let router = express.Router();
 let userController = require("../controllers/user.controller");
 router.get("/",userController.get);
 router.get("/create-user",userController.createForm);
-router.post("/create-user",userController.save);
+router.post("/create-user/add",userController.save);
 
 module.exports = router;
